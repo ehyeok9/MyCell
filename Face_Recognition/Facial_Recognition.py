35a09a023c588c8aac2d210cd3b7a43333633524
Jaeman Jang <woak1010@kookmin.ac.kr>
2019-11-21 13:15:17 +0900

    v.3

--- Face_Recognition/Facial_Recognition.py
+++ Face_Recognition/Facial_Recognition.py
@@ -30,7 +30,43 @@ class Face_Recognition:
 
         self.face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    def face_detector(self, img, size = 0.5):
+    def face_extractor(self, img):
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = self.face_classifier.detectMultiScale(gray, 1.3, 5)
+
+        if faces is ():
+            return None
+
+        for (x, y, w, h) in faces:
+            cropped_face = img[y:y + h, x:x + w]
+
+        return cropped_face
+
+    def capture_face(self):
+        cap = cv2.VideoCapture(0)
+        count = 0
+
+        while True:
+            ret, frame = cap.read()
+            if self.face_extractor(frame) is not None:
+                count += 1
+                face = cv2.resize(self.face_extractor(frame), (200, 200))
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+                file_name_path = 'userFaces/user' + str(count) + '.jpg'
+                cv2.imwrite(file_name_path, face)
+
+                cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                cv2.imshow('Face Cropper', face)
+            else:
+                print("Face not Found")
+                pass
+
+            if cv2.waitKey(1) == 13 or count == 100:
+                break
+
+    def face_detector(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_classifier.detectMultiScale(gray,1.3,5)
 
@@ -76,7 +112,7 @@ class Face_Recognition:
 
         matplotlib.rc('font', family='NanumGothic')
 
-        obj.bar(index, y1_value, tick_label=x_name, align='center')
+        plt.bar(index, y1_value, tick_label=x_name, align='center')
 
         plt.xlabel('Name')
         plt.ylabel('Confidence (%)')
@@ -89,7 +125,6 @@ if __name__ == "__main__":
     f = Face_Recognition()
     dic = f.compare_face()
     f.draw_graph(dic)
-
 
 """
 cap = cv2.VideoCapture(0)
