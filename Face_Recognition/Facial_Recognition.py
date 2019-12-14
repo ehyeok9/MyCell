68e275a6574b7471b7c75d24faeeb51229db6a27
Jaeman Jang <woak1010@kookmin.ac.kr>
2019-12-14 15:13:06 +0900

    v.4

--- Face_Recognition/Facial_Recognition.py
+++ Face_Recognition/Facial_Recognition.py
@@ -105,6 +105,8 @@ class FaceRecognition:
 
 class FaceCapture:
 
+    count = 0
+
     @staticmethod
     def face_extractor(img):
         face_classifier = cv2.CascadeClassifier(data_path[2] + 'haarcascade_frontalface_default.xml')
