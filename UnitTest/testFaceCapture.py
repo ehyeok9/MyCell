6ce9f8deef61b72077a8319aeaaeacee57b0980c
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2019-12-19 00:02:30 +0900

    ada

---
+++ UnitTest/testFaceCapture.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+lst = ["/home/user/", "/home/ehyeok9/github/"]
+directory = lst[1]
+sys.path.insert(0, directory + "Software-Project-II---AD-project/Face_Recognition")
+from Facial_Recognition import FaceRecognition, FaceCapture
+from PyQt5 import *
+import numpy as np
+from PIL import Image
+
+class testFaceCapture(unittest.TestCase):
+
+    def setUp(self):
+        self.face = FaceCapture
+        self.image = Image.open("/home/ehyeok9/github/Software-Project-II---AD-project/Face_Recognition/othersFaces/menFaces
+")
+    def testFaceExtractor(self):
+        self.assertIsNotNone(self.face.face_extractor(self.image))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ UnitTest/testFaceRecognition.py
@@ -0,0 +1,28 @@
+import unittest
+import sys
+lst = ["/home/user/", "/home/ehyeok9/github/"]
+directory = lst[1]
+sys.path.insert(0, directory + "Software-Project-II---AD-project/Face_Recognition")
+from Facial_Recognition import FaceRecognition, FaceCapture
+from PyQt5 import *
+
+class testFaceRecognition(unittest.TestCase):
+
+    def setUp(self):
+        self.face = FaceRecognition("이혁규", "man")
+
+
+    def testVariale(self):
+        self.assertEqual(self.face.username, "이혁규")
+        self.assertEqual(self.face.usergender, "man")
+        self.assertTrue(self.face.isMan)
+
+    def testUpdatData(self):
+        self.assertEqual(len(self.face.userFiles), 100)
+
+    def testCompareFace(self):
+        self.assertIsNotNone(self.face.compare_face())
+        
+
+if __name__ == '__main__':
+    unittest.main()
